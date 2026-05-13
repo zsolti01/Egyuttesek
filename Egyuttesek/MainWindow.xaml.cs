d8de73b3fc8710b431877c1758cab3446fa03967
vecseizs <vecseizs@kkszki.hu>
2026-05-13 09:42:45 +0200

    Kesz a project

--- Egyuttesek/MainWindow.xaml.cs
+++ Egyuttesek/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,7 @@ namespace Egyuttesek
         public MainWindow()
         {
             InitializeComponent();
+            LoadData();
         }
 
         private void kilepesButton_Click(object sender, RoutedEventArgs e)
@@ -31,5 +34,60 @@ namespace Egyuttesek
         }
 
         public string connectionString = "server=localhost;user=root;password=;database=vizsga;";
+
+        public void LoadData()
+        {
+            var conn = new MySqlConnection(connectionString);
+
+            conn.Open();
+
+            string sql = "SELECT * FROM albumok";
+
+            var cmd = new MySqlCommand(sql, conn);
+
+            var adapter = new MySqlDataAdapter(cmd);
+
+            var dt = new DataTable();
+
+            adapter.Fill(dt);
+
+            dataG1.ItemsSource = dt.DefaultView;
+
+            conn.Close();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            var row = dataG1.SelectedItem as DataRowView;
+
+            var conn = new MySqlConnection(connectionString);
+
+            conn.Open();
+
+            string sql = "SELECT COUNT(*) FROM `zeneszek` WHERE `egyuttes` = @egyuttes";
+
+            var cmd = new MySqlCommand(sql, conn);
+
+            cmd.Parameters.AddWithValue("@egyuttes", row["egyuttes"]);
+
+            MessageBox.Show(cmd.ExecuteScalar().ToString());
+
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var conn = new MySqlConnection(connectionString);
+
+            conn.Open();
+
+            string sql = "SELECT `album` FROM ˙albumok` ORDER BY `hossz` DESC LIMIT 1";
+            
+            var cmd = new MySqlCommand(sql, conn);
+
+            MessageBox.Show(cmd.ExecuteScalar().ToString());
+
+            conn.Close();
+        }
     }
 }
